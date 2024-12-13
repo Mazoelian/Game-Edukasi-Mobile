9b6b7e4528e5c779ae0b74a026634fb0aba87957
arzaqqq <mmiftahularzaq01@gmail.com>
2024-12-13 16:01:09 +0700

    tes commit

--- app/src/main/java/com/example/uas_gameedukasi_kelompok7/SplashActivity.kt
+++ app/src/main/java/com/example/uas_gameedukasi_kelompok7/SplashActivity.kt
@@ -2,6 +2,7 @@ package com.example.uas_gameedukasi_kelompok7
 
 import android.content.Intent
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -17,6 +18,8 @@ class SplashActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_splash)
 
+
+
         // Mengatur padding untuk sistem bar
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -29,6 +32,13 @@ class SplashActivity : AppCompatActivity() {
             delay(splashDelay)
             moveToMainActivity()
         }
+
+        // Membuat status bar transparan
+        window.decorView.systemUiVisibility = (
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        or View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                )
+        window.statusBarColor = android.graphics.Color.TRANSPARENT
     }
 
     // Fungsi untuk berpindah ke MainActivity
@@ -37,4 +47,7 @@ class SplashActivity : AppCompatActivity() {
         startActivity(intent)
         finish() // Tutup SplashActivity
     }
+
+
+
 }
